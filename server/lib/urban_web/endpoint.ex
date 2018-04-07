e72b97ea969ec6ba825f03210bf65a8059d8a080
maneptha <maneptha@gmail>
2018-04-07 21:10:36 +0200

    Mix corsica needed again to handle CORS.

--- server/lib/urban_web/endpoint.ex
+++ server/lib/urban_web/endpoint.ex
@@ -46,11 +46,11 @@ defmodule UrbanWeb.Endpoint do
     signing_salt: "Cwgxzc6E"
   )
 
-  # plug(
-  #   Corsica,
-  #   origins: "*",
-  #   allow_headers: ~w(Accept Content-Type Authorization Origin)
-  # )
+  plug(
+    Corsica,
+    origins: "*",
+    allow_headers: ~w(Accept Content-Type Authorization Origin)
+  )
 
   plug(UrbanWeb.Redictor)
   plug(UrbanWeb.Router)
