d1d5c45bf2e57d511ddcb49534c9bca34e374a84
maneptha <maneptha@gmail>
2018-04-14 08:26:02 +0200

    Production build

--- server/config/config.exs
+++ server/config/config.exs
@@ -31,11 +31,7 @@ config :ex_admin,
     UrbanWeb.ExAdmin.Itinerary
   ]
 
-config :arc,
-  storage: Arc.Storage.GCS,
-  bucket: "urban-travel-plans"
-
-# config :arc, storage: Arc.Storage.Local
+config :arc, storage: Arc.Storage.Local
 config :goth, json: "./1523607878351" |> File.read!()
 
 # Import environment specific config. This must remain at the bottom
